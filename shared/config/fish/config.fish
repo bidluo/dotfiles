aa17098dc47996b96a09c485dc6f8c01b8d55b1f
David Liaw <david@liaw.me>
2026-03-26 16:44:44 +1100

    Updated configs

--- shared/config/fish/config.fish
+++ shared/config/fish/config.fish
@@ -4,4 +4,8 @@ fish_ssh_agent
 set PATH ~/.local/bin $PATH
 set -gx PATH /sbin /usr/sbin ~/.local/bin $PATH
 
-/Users/david/.local/bin/mise activate fish | source
+set -gx GPG_TTY (tty)
+set -gx DIRENV_LOG_FORMAT 
+direnv hook fish | source
+
+#/Users/david/.local/bin/mise activate fish | source
